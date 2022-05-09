8ebd67347956fd2a45a4621d14f4c5e41f1effe3
Rodrigo Monteiro <rodrigomonteiiro17@gmail.com>
2022-05-09 14:57:11 -0300

    day 1

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { Widget } from "./components/Widget";
+
+export function App(){
+  return(
+    <Widget></Widget>
+    )
+    
+  }
+  
+  // interface ButtonPros {
+  //   text: string;
+  // }
+  // function Button(props: ButtonPros) {
+  //   return <button className="button bg-violet-500 rounded hover:bg-violet-700">{props.text}</button>;
+  // }
+  // function App() {
+  //   return (
+  //     <div className="flex gap-2">
+  //       <Button text="Send"></Button>
+  //       <Button text="Cancel"></Button>
+  //     </div>
+  //   );
+  // }
+  // export default App;
